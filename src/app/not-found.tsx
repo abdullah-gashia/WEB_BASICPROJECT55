a90b8aecbacc86b6399e9d5f4380a6e039588524
abdullah-gashia <s6630613012@phuket.psu.ac.th>
2024-12-05 17:04:39 +0700

    update error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -17,7 +17,7 @@ export default function PageNotFound({}: Props) {
         src='https://wallpapers-clan.com/wp-content/uploads/2022/09/madara-pfp-20.jpg'
         alt='Error Bro'
         width={200}
-        className='border border-black border-2 m-4'
+        className='border border-black m-4'
       />
       <h1 className='text-red-600 text-xl m-4'>
         ว้าว เก่งมากเลยนะ ที่จะทำแบบนี้55555
@@ -26,12 +26,11 @@ export default function PageNotFound({}: Props) {
         Please doesn't skibidi toilet with me
       </h1>
 
-      {/* ปุ่มสำหรับกลับไปหน้าแรก */}
       <button
         onClick={handleClick}
         className='mt-6 px-4 py-2 bg-red-500 text-white rounded hover:bg-blue-700 transition-all'
       >
-        กลับไปหน้าแรก
+        Home Page
       </button>
     </div>
   );
